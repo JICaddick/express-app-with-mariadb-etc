02cc75d7f4a546a53c2478b8a35d2ab7be6bb6e7
JICaddick <james_caddick16@hotmail.com>
2023-03-13 16:08:31 -0500

    middleware error

--- app.js
+++ app.js
@@ -1,32 +1,44 @@
-var createError = require('http-errors');
+"use strict";
+// NEEDED? const indexRouter = require('./routes/index'); NEEDED? 
+exports.__esModule = true;
+// const router = express.Router(); now in routes/index.js as const router = Router(); using const { Router } = require('express'); on line 1
+// const requiresAuth = require('express-openid-connect').requiresAuth; and const auth = require('express-openid-connect'); now in routes/index.js as `const { auth, requiresAuth } = require('express-openid-connect');`
+// const express = require('express'); moved to routes/index.js as const { Router } = require('express');
+// import { auth, requiresAuth } from 'express-openid-connect'; moved to routes/index.js as const { auth, requiresAuth } = require('express-openid-connect');
+// const authConfig and dotenv.config(); moved to routes/index.ts
+// the following `router.use(auth(authConfig))` was in there originally.
+// req.isAuthenticated is provided from the auth router
 var express = require('express');
+var index_1 = require("./routes/index");
+var createError = require('http-errors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+// NEEDED?  app.use('/', indexRouter); app.use('/users', usersRouter); NEEDED? 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', index_1["default"]);
+app.use('/login', index_1["default"]);
+app.use('/aboutme', index_1["default"]);
+app.use('/discoveramagicalworldofpotatoes', index_1["default"]);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     next(createError(404));
 });
 // error handler
+// set locals, only providing error in development
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
     // render the error page
     res.status(err.status || 500);
     res.render('error');
 });
-module.exports = app;
+module.exports = index_1["default"];
--- app.ts
+++ app.ts
@@ -1,49 +1,22 @@
-// import { auth, requiresAuth } from 'express-openid-connect';
-// import dotenv from 'dotenv';
-import express from 'express';
-import router from './routes/index';
+// NEEDED? const indexRouter = require('./routes/index'); NEEDED? 
 
-// const express = require('express');
-// const indexRouter = require('./routes/index');
-// const auth = require('express-openid-connect');
-// const dotenv = require('dotenv');
-// const requiresAuth = require('express-openid-connect').requiresAuth;
+// const router = express.Router(); now in routes/index.js as const router = Router(); using const { Router } = require('express'); on line 1
 
-// dotenv.config();
+// const requiresAuth = require('express-openid-connect').requiresAuth; and const auth = require('express-openid-connect'); now in routes/index.js as `const { auth, requiresAuth } = require('express-openid-connect');`
+// const express = require('express'); moved to routes/index.js as const { Router } = require('express');
+// import { auth, requiresAuth } from 'express-openid-connect'; moved to routes/index.js as const { auth, requiresAuth } = require('express-openid-connect');
 
-// const authConfig = {
-//   authRequired: false,
-//   auth0Logout: true,
-//   baseURL: process.env.baseURL,
-//   clientID: process.env.clientID,
-//   issuerBaseURL: process.env.issuerBaseURL,
-//   secret: process.env.secret,
-// };
-
-// const router = express.Router();
-
-// the following router.use was in there originally.
-// router.use(auth(authConfig));
-
-// Attempt #1 (with const authconfig) auth router attaches /login, /logout, and /callback routes to the baseURL
-// app.use(auth(authConfig));
-
+// const authConfig and dotenv.config(); moved to routes/index.ts
+// the following `router.use(auth(authConfig))` was in there originally.
 // req.isAuthenticated is provided from the auth router
 
-// router.get('/', (req, res) => {
-//   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-// });
-
-// router.get('/login', requiresAuth(), (req, res) => {
-//   res.send(JSON.stringify(req.openid.user));
-// });
+import express from 'express';
+import router from './routes/index';
 
 var createError = require('http-errors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-
 
 var app = express();
 
@@ -51,18 +24,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// NEEDED?  app.use('/', indexRouter); app.use('/users', usersRouter); NEEDED? 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// router.get('/login', requiresAuth(), (req, res) => {
-//   res.send(JSON.stringify(req.openid.user));
-// });
-
 app.use('/', router);
-// app.use('/users', usersRouter);
 app.use('/login', router);
 app.use('/aboutme', router);
 app.use('/discoveramagicalworldofpotatoes', router);
